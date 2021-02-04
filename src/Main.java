f25eab64db6e0ef464271a74af19ffb67c0e24ba
PabloViniegra <pablovpmadrid@gamil.com>
2021-02-04 17:18:46 +0100

    segundo ejercicio servidor

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,23 @@ import java.net.SocketException;
 public class Main {
     public static void main(String[] args) throws IOException {
         DatagramSocket datagramSocket = null;
+
         try {
             datagramSocket = new DatagramSocket(8080);
         } catch (SocketException e) {
             e.printStackTrace();
         }
         byte[] receive = new byte[65535];
-        DatagramPacket datagramPacket;
-        String textReceived;
-        while (true) {
-            datagramPacket = new DatagramPacket(receive, receive.length);
-            datagramSocket.receive(datagramPacket);
-            textReceived = new String(receive);
-            System.out.println("Client: " + textReceived);
 
-            if (textReceived.equals("*")) {
-                break;
-            }
-            receive = new byte[65535];
-        }
+
+
+        DatagramPacket datagramPacket = new DatagramPacket(receive, receive.length);
         assert datagramSocket != null;
+        datagramSocket.receive(datagramPacket);
+        String textReceived = new String(receive);
+        System.out.println("Client: " + textReceived);
+
+
         datagramSocket.close();
     }
 
